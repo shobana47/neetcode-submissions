d4073086f5d572af7b12d2d21339bb1dfd518d1b
SHOBANA G <shobikrishnan2005@gmail.com>
2026-05-30 18:39:16 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        char []S=s.toCharArray();
+        char[] T=t.toCharArray();
+        Arrays.sort(S);
+        Arrays.sort(T);
+        return Arrays.equals(S,T);
+    }
+}
