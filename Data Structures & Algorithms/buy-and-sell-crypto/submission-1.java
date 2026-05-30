6f224cfa3cbc0f3d90fe5b5c758db1f4a244ab42
SHOBANA G <shobikrishnan2005@gmail.com>
2026-05-30 18:09:11 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        int minprice=prices[0];
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]<minprice)
+            {
+                minprice=prices[i];
+            }
+            int profit=prices[i]-minprice;
+            if(profit>maxprofit)
+            {
+                maxprofit=profit;
+            }
+        }
+        return maxprofit;
+    }
+}
